db6ca5201a4c8107e39f6cb05df443384f652b37
ebubekeyz <ebubeofforjoe@gmail.com>
2025-03-24 10:08:45 +0100

    4

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const helmet = require('helmet');
 let originUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:5173'
-    : 'https://trustunion-bank.netlify.app'
+    : 'https://trustunion-bank.com'
 
 // let originUrl =
 //   process.env.NODE_ENV !== 'production'
